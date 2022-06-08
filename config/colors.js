7d4d30e34c3e56acef1bc2444e88398b12d3e40e
ToniIvars <toniivaju@gmail.com>
2022-06-08 20:11:11 +0200

    Initial commit

---
+++ config/colors.js
@@ -0,0 +1,5 @@
+export default {
+  dark: '#424242',
+  darker: '#2B2B2B',
+  light: '#F7F7FF'
+}
---
+++ screens/MainScreen.js
@@ -0,0 +1,49 @@
+import { StyleSheet, Text, SafeAreaView, View, Platform, StatusBar} from 'react-native'
+import colors from '../config/colors'
+
+export default function MainScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={[styles.basic, styles.configBar]}>
+        <Text style={styles.text}>This will be the bar</Text>
+      </View>
+
+      <View style={[styles.basic, styles.currentEvent]}>
+        <Text style={styles.text}>This will be the current event</Text>
+      </View>
+
+      <View style={[styles.basic, styles.nextEvent]}>
+        <Text style={styles.text}>This will be the information for the next event</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+  
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
+  },
+  basic: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+  },
+  configBar: {
+    flex: 1,
+    backgroundColor: colors.dark,
+  },
+  currentEvent: {
+    flex: 10,
+    backgroundColor: colors.darker,
+  },
+  nextEvent: {
+    flex: 1,
+    backgroundColor: colors.dark,
+  },
+  text: {
+    color: colors.light
+  }
+})
