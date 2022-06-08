0441753ab851826a735b0f343290e52006464c1a
ToniIvars <toniivaju@gmail.com>
2022-06-08 20:40:06 +0200

    Design the config bar

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,11 +1,16 @@
 import { StyleSheet, Text, SafeAreaView, View, Platform, StatusBar} from 'react-native'
+import OctIcon from 'react-native-vector-icons/Octicons'
+import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons'
+
 import colors from '../config/colors'
 
 export default function MainScreen() {
   return (
     <SafeAreaView style={styles.container}>
       <View style={[styles.basic, styles.configBar]}>
-        <Text style={styles.text}>This will be the bar</Text>
+        <OctIcon name='gear' size={24} style={[styles.text, {paddingTop: 2}]} />
+        <Text style={[styles.text, styles.bigText]}>Timetable title</Text>
+        <MaterialIcon name='timetable' size={28} style={[styles.text, {paddingTop: 6}]} />
       </View>
 
       <View style={[styles.basic, styles.currentEvent]}>
@@ -33,7 +38,10 @@ const styles = StyleSheet.create({
   },
   configBar: {
     flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     backgroundColor: colors.dark,
+    paddingHorizontal: 20
   },
   currentEvent: {
     flex: 10,
@@ -44,6 +52,9 @@ const styles = StyleSheet.create({
     backgroundColor: colors.dark,
   },
   text: {
-    color: colors.light
-  }
+    color: colors.light,
+  },
+  bigText: {
+    fontSize: 28
+  },
 })
