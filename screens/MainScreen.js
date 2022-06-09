22cfeb428721666cccdaf1069c71b3b1a20b5da6
ToniIvars <toniivaju@gmail.com>
2022-06-09 12:57:48 +0200

    Design currentEvent and nextEvent section

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -9,16 +9,18 @@ export default function MainScreen() {
     <SafeAreaView style={styles.container}>
       <View style={[styles.basic, styles.configBar]}>
         <OctIcon name='gear' size={24} style={[styles.text, {paddingTop: 2}]} />
-        <Text style={[styles.text, styles.bigText]}>Timetable title</Text>
+        <Text style={[styles.text, styles.timetableTitle]}>Timetable title</Text>
         <MaterialIcon name='timetable' size={28} style={[styles.text, {paddingTop: 6}]} />
       </View>
 
       <View style={[styles.basic, styles.currentEvent]}>
-        <Text style={styles.text}>This will be the current event</Text>
+        <Text style={[styles.text, styles.clock]}>00:00</Text>
+        <Text style={[styles.text, styles.currentEventName]}>This will be the event name</Text>
       </View>
 
       <View style={[styles.basic, styles.nextEvent]}>
-        <Text style={styles.text}>This will be the information for the next event</Text>
+        <OctIcon name='chevron-right' size={24} style={[styles.text, {marginRight: 10}]} />
+        <Text numberOfLines={1} style={styles.text}>This will be the information for the next event</Text>
       </View>
     </SafeAreaView>
   )
@@ -36,6 +38,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     width: '100%',
   },
+  text: {
+    color: colors.foreground,
+  },
   configBar: {
     flex: 1,
     flexDirection: 'row',
@@ -43,18 +48,27 @@ const styles = StyleSheet.create({
     backgroundColor: colors.secondaryBackgorund,
     paddingHorizontal: 20
   },
+  timetableTitle: {
+    fontSize: 28
+  },
   currentEvent: {
     flex: 10,
     backgroundColor: colors.mainBackground,
+    paddingBottom: 70
+  },
+  currentEventName: {
+    fontSize: 20,
+    textAlign: 'center',
+    paddingHorizontal: 24
+  },
+  clock: {
+    fontSize: 112
   },
   nextEvent: {
     flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
     backgroundColor: colors.secondaryBackgorund,
-  },
-  text: {
-    color: colors.foreground,
-  },
-  bigText: {
-    fontSize: 28
+    paddingHorizontal: 16
   },
 })
